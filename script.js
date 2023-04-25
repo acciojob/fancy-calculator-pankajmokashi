c5ac87521b78c64ef5aed025e2db056f007d0f19
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-25 21:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+//your JS code here. If required./
+const num1 = parseInt(prompt());
+const num2 = parseInt(prompt());
+result = num1 * 2 + num2
+alert("The final result is: " + result)
